ded26ee1c8f841b4745424a6a9c002ca69f5f932
Marcelo Torres <marcelotorres1982@gmail.com>
2024-10-19 22:28:19 -0300

    Update app.py
    Add comments

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+# Carregar dados
 df = pd.read_csv("pizzas.csv")
 
+# Treinar modelo de regressão linear
 modelo = LinearRegression()
 x = df[["diametro"]]
 y = df[["preco"]]
 
 modelo.fit(x, y)
-
+# Configurar Streamlit
 st.title("Prevendo o valor da Pizza")
 st.divider()
 
+# Entrada do usuário
 diametro = st.number_input("Digite o tamanho da pizza: ")
 
+# Previsão do preço
 if diametro:
     preco_previsto = modelo.predict([[diametro]])[0][0]
     st.write(f"O valor da Pizza com diâmetro de {
